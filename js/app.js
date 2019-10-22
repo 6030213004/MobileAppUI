d503d592a241f91749a414a7ea7bb45c02f14e0b
6030213004 <47880935+6030213004@users.noreply.github.com>
2019-10-22 20:32:22 +0700

    foodjobs 1.7
    90% done

--- js/app.js
+++ js/app.js
@@ -254,29 +254,15 @@ document.addEventListener('init', function (event) {
       $("#content")[0].load("home.html");
     });
 
-    $("#gopay").click(function () {
-      console.log('go to payment page');
-      $("#content")[0].load("payment.html");
+    $("#confirmbtn").click(function () {
+      console.log('confirmed');
+      $("#content")[0].load("successfull.html");
     });
 
 
   }
 
-  if (page.id === 'paymentpage') {
-    console.log("payment page");
-
-    $("#backtoconfirm").click(function () {
-      console.log('back to confirm page');
-      $("#content")[0].load("confirmorder.html");
-    });
-
-    $("#cod").click(function () {
-      console.log('cash on delivery');
-      $("#content")[0].load("status.html");
-    });
-
-
-  }
+  
 
   if (page.id === 'profile') {
     console.log("profile page");
@@ -289,16 +275,6 @@ document.addEventListener('init', function (event) {
 
   }
 
-  if (page.id === 'statuspage') {
-    console.log("status page");
-
-    $("#finish").click(function () {
-      console.log('back to home page');
-      $("#content")[0].load("home.html");
-    });
-
-
-  }
 
 
   db.collection("recommended").get().then((querySnapshot) => {
@@ -472,7 +448,7 @@ function displayCart() {
             "<td><div class='input-group'><input type='number' class='item-count form-control' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>" +
             "<button class='minus-item input-group-addon btn btn-primary' data-name=" + cartArray[i].name + ">-</button>" +
             "<button class='plus-item btn btn-primary input-group-addon' data-name=" + cartArray[i].name + ">+</button></div></td>" +
-            "<td><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">X</button></td>" +
+            // "<td><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">X</button></td>" +
             " = " +
             "<td>" + cartArray[i].total + "</td>" +
             "</tr>";
